b6ed7c7e1e13ae57eea3f03dba1a75c2ad274c94
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2024-07-16 12:52:27 +0200

    handle string request, add default behaviour for install_resolver

--- src/bdd_dsl/utils/resolver.py
+++ src/bdd_dsl/utils/resolver.py
@@ -1,9 +1,17 @@
 # Inspired by https://github.com/comp-rob2b/kindyngen/ (kindyngen.utility.resolver)
 # SPDX-License-Identifier: MPL-2.0
+from typing import Optional
+from os.path import join
+import platformdirs
 import pathlib
 import urllib.request
 import urllib.response
 from email.message import EmailMessage
+from bdd_dsl.models.uri import URL_SECORO, URL_COMP_ROB2B
+from bdd_dsl.utils.common import BDD_DSL_VERSION
+
+
+__PKG_CACHE_ROOT = join(platformdirs.user_cache_dir(), "bdd-dsl")
 
 
 class IriToFileResolver(urllib.request.OpenerDirector):
@@ -11,7 +19,7 @@ class IriToFileResolver(urllib.request.OpenerDirector):
     A `urllib.request.OpenerDirector` that remaps specific URLs to local files.
     """
 
-    def __init__(self, url_map: dict, download: bool = False):
+    def __init__(self, url_map: dict, download: bool = True):
         """
         A key-value pair in `url_map` specifies a prefix of a URL to a local location.
         For example, `{ "http://example.org/": "foo/bar/" }` would remap any urllib open request
@@ -26,11 +34,17 @@ class IriToFileResolver(urllib.request.OpenerDirector):
         self._empty_header = EmailMessage()  # header expected by addinfourl
 
     def open(self, fullurl, data=None, timeout=None):
-        assert isinstance(
-            fullurl, urllib.request.Request
-        ), f"expected URL of type 'urllib.request.Request', got type '{type(fullurl)}'"
+        if isinstance(fullurl, str):
+            url_req = urllib.request.Request(fullurl)
+            url_req.add_header("User-Agent", f"bdd-dsl/{BDD_DSL_VERSION}")
+        elif isinstance(fullurl, urllib.request.Request):
+            url_req = fullurl
+        else:
+            raise RuntimeError(
+                f"expected URL of type 'str' or 'urllib.request.Request', got type '{type(fullurl)}'"
+            )
 
-        url = pathlib.Path(fullurl.full_url)
+        url = pathlib.Path(url_req.full_url)
 
         # If the requested URL starts with any key in the url_map, fetch the file from a
         # local file that is derived from the URL and the value in the map
@@ -52,26 +66,41 @@ class IriToFileResolver(urllib.request.OpenerDirector):
                     parent_path.mkdir(parents=True)
                 assert parent_path.is_dir(), f"not a directory: {parent_path}"
 
-                with self.default_opener.open(fullurl.full_url) as url_data:
+                with self.default_opener.open(url_req, data, timeout) as url_data:
                     with path.open("wb") as cache_file:
                         cache_file.write(url_data.read())
-                assert path.exists(), f"File '{path}' not cached for URL '{fullurl.full_url}'"
+                assert path.exists(), f"File '{path}' not cached for URL '{url_req.full_url}'"
 
             # Open the file and wrap it in an urllib response
             fp = path.open("rb")
             resp = urllib.response.addinfourl(
-                fp, headers=self._empty_header, url=fullurl.full_url, code=200
+                fp, headers=self._empty_header, url=url_req.full_url, code=200
             )
             return resp
 
         # If we did not find any match above just continue with the default opener
         # which has the behaviour as initially expected by rdflib.
-        return self.default_opener.open(fullurl, data, timeout)
+        return self.default_opener.open(url_req, data, timeout)
 
 
-def install(resolver):
+def install_resolver(
+    resolver: Optional[urllib.request.OpenerDirector] = None,
+    url_map: Optional[dict] = None,
+    download: bool = True,
+):
     """
-    Note that only a single opener can be globally installed in urllib. Only the
-    latest installed resolver will be active.
+    Note that only a single opener can be globally installed in urllib.
+    Only the latest installed resolver will be active.
+    If no `resolver` is specified, the default behaviour using `IriToFileResolver` is to
+    download the requested files to the user cache directory using `platformdirs`.
+    For Linux this should be `$HOME/.cache/bdd-dsl/`.
     """
+    if resolver is None:
+        if url_map is None:
+            url_map = {
+                URL_SECORO: join(__PKG_CACHE_ROOT, "secoro"),
+                URL_COMP_ROB2B: join(__PKG_CACHE_ROOT, "comp-rob2b"),
+            }
+        resolver = IriToFileResolver(url_map=url_map, download=download)
+
     urllib.request.install_opener(resolver)
