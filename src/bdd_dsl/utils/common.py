4caea32c5f36cd331990260b7cad354109cdef84
Minh Nguyen <minh.nguyen@uni-bremen.de>
2024-07-26 01:22:02 +0200

    move file and caching funcs to rdf_utils

--- src/bdd_dsl/utils/common.py
+++ src/bdd_dsl/utils/common.py
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier:  GPL-3.0-or-later
 import re
 import signal
-from socket import _GLOBAL_DEFAULT_TIMEOUT
-import urllib.request
 from importlib.metadata import version
 
 import numpy as np
@@ -14,48 +12,7 @@ __FILENAME_REPLACEMENTS = {" ": "_", ":": "__", "/": "_"}
 __VAR_NAME_REPLACEMENTS = {"-": "_"}
 __VAR_NAME_REPLACEMENTS.update(__FILENAME_REPLACEMENTS)
 
-__FILE_LOADER_CACHE = {}
-__URL_CONTENT_CACHE = {}
-
 BDD_DSL_VERSION = version("bdd-dsl")
-
-
-def read_file_and_cache(filepath: str) -> str:
-    """Read and cache string contents of files for quick access and reducing IO operations.
-
-    May need "forgetting" mechanism if too many large files are stored. Should be fine
-    for loading JSON metamodels and SHACL constraints in Turtle format.
-    """
-    if filepath in __FILE_LOADER_CACHE:
-        return __FILE_LOADER_CACHE[filepath]
-
-    with open(filepath) as infile:
-        file_content = infile.read()
-
-    if isinstance(file_content, bytes):
-        file_content = file_content.decode("utf-8")
-
-    __FILE_LOADER_CACHE[filepath] = file_content
-    return file_content
-
-
-def read_url_and_cache(url: str, timeout=_GLOBAL_DEFAULT_TIMEOUT) -> str:
-    """Read and cache text responses from URL
-
-    `timeout` specifies duration in seconds to wait for response. Only works for HTTP, HTTPS & FTP.
-    By default `socket._GLOBAL_DEFAULT_TIMEOUT` will be used, which usually means no timeout.
-    """
-    if url in __URL_CONTENT_CACHE:
-        return __URL_CONTENT_CACHE[url]
-
-    with urllib.request.urlopen(url, timeout=timeout) as f:
-        url_content = f.read()
-
-    if isinstance(url_content, bytes):
-        url_content = url_content.decode("utf-8")
-
-    __URL_CONTENT_CACHE[url] = url_content
-    return url_content
 
 
 def get_valid_name(name: str, replacement_dict: dict) -> str:
