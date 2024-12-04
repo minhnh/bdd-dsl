bb271c9d0f60b9500fb6944ebdd5bd00d6085e27
Minh Nguyen <minh.nguyen@uni-bremen.de>
2024-12-04 21:54:35 +0100

    adapt execution model to new when behaviour clause
    - ExecutionModel load BehaviourImplModel by ScenarioVariantModel instead
      of Behaviour URI
    - allow specifying additional loaders for behaviour implementations

--- src/bdd_dsl/execution/common.py
+++ src/bdd_dsl/execution/common.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier:  GPL-3.0-or-later
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 from behave.runner import Context
-from rdf_utils.models.common import ModelBase, ModelLoader
+from rdf_utils.models.common import AttrLoaderProtocol, ModelBase, ModelLoader, get_node_types
 from rdflib import Graph, URIRef
 from rdf_utils.uri import URL_SECORO_M
 from rdf_utils.caching import read_url_and_cache
@@ -11,7 +11,12 @@ from rdf_utils.models.python import (
     import_attr_from_model,
     load_py_module_attr,
 )
-from bdd_dsl.models.urirefs import URI_BDD_TYPE_BHV_IMPL, URI_BHV_PRED_OF_BHV
+from bdd_dsl.models.urirefs import (
+    URI_BDD_PRED_OF_VARIANT,
+    URI_BHV_PRED_OF_BHV,
+    URI_BDD_PRED_HAS_BHV_IMPL,
+)
+from bdd_dsl.models.user_story import ScenarioVariantModel
 
 
 URL_Q_SCENARIO_EXEC = f"{URL_SECORO_M}/acceptance-criteria/bdd/queries/scenario-execution.rq"
@@ -37,55 +42,80 @@ class Behaviour(ABC):
 
 class BehaviourImplModel(ModelBase):
     behaviour: Optional[Behaviour]
+    behaviour_uri: URIRef
+    behaviour_types: set[URIRef]
 
-    def __init__(self, graph: Graph, bhv_impl_uri: URIRef) -> None:
-        super().__init__(graph=graph, node_id=bhv_impl_uri)
+    def __init__(self, graph: Graph, bhv_impl_id: URIRef) -> None:
+        super().__init__(graph=graph, node_id=bhv_impl_id)
         self.behaviour = None
 
-        assert URI_BDD_TYPE_BHV_IMPL in self.types
+        bhv_uri = graph.value(subject=self.id, predicate=URI_BHV_PRED_OF_BHV)
+        assert isinstance(
+            bhv_uri, URIRef
+        ), f"BehaviourImpl '{self.id}' doesnn't ref a Behaviour URI: {bhv_uri}"
+        self.behaviour_uri = bhv_uri
+
+        self.behaviour_types = get_node_types(graph=graph, node_id=self.behaviour_uri)
+        assert len(self.behaviour_types) > 0, f"No types found for Behaviour '{self.behaviour_uri}'"
 
 
 class ExecutionModel(object):
+    _bhv_impl_by_scr_var: dict[URIRef, BehaviourImplModel]
+    _scr_exec_by_scr_var: dict[URIRef, ModelBase]
+    _scr_exec_graph: Graph
     bhv_model_loader: ModelLoader
-    _scenario_exec_graph: Graph
-    _bhv_impl: Dict[URIRef, BehaviourImplModel]
 
-    def __init__(self, graph: Graph) -> None:
+    def __init__(
+        self, graph: Graph, bhv_loaders: list[AttrLoaderProtocol] = [load_py_module_attr]
+    ) -> None:
         g_query_str = read_url_and_cache(URL_Q_SCENARIO_EXEC)
         q_result = graph.query(g_query_str)
-        assert q_result.type == "CONSTRUCT"
-        assert q_result.graph is not None
-
-        self._scenario_exec_graph = q_result.graph
-
-        for prefix, uri in graph.namespaces():
-            self._scenario_exec_graph.bind(prefix=prefix, namespace=uri, override=True)
-
-        self._bhv_impl = {}
-        self.bhv_model_loader = ModelLoader()
-        self.bhv_model_loader.register(load_py_module_attr)
-
-    @property
-    def scenario_exec_graph(self) -> Graph:
-        return self._scenario_exec_graph
-
-    def load_behaviour_impl(
-        self, context: Context, bhv_id: URIRef, override: bool = False, **kwargs: Any
-    ) -> BehaviourImplModel:
-        if bhv_id in self._bhv_impl and not override:
-            return self._bhv_impl[bhv_id]
-
-        bhv_impl_ids = list(
-            self._scenario_exec_graph.subjects(object=bhv_id, predicate=URI_BHV_PRED_OF_BHV)
-        )
         assert (
-            len(bhv_impl_ids) == 1
-        ), f"not exactly 1 implementation found for '{bhv_id}', found: {bhv_impl_ids}"
-        assert isinstance(bhv_impl_ids[0], URIRef)
+            q_result.type == "CONSTRUCT" and q_result.graph is not None
+        ), "ScenarioExecution query did not return a graph"
+        assert len(q_result.graph) > 0, "ScenarioExecution query returns an empty graph"
+        self._scr_exec_graph = q_result.graph
 
+        self._bhv_impl_by_scr_var = {}
+        self._scr_exec_by_scr_var = {}
+
+        self.bhv_model_loader = ModelLoader()
+        for loader in bhv_loaders:
+            self.bhv_model_loader.register(loader=loader)
+
+    def load_behaviour_impl(self, context: Context, **kwargs: Any) -> BehaviourImplModel:
         assert hasattr(context, "model_graph"), "'model_graph' not added to behave context"
+        assert isinstance(
+            context.model_graph, Graph
+        ), "load_bhv_impl: 'model_graph' not a rdflib.Graph"
+        assert hasattr(
+            context, "current_scenario"
+        ), "'current_scenario' not added to behave context"
+        assert isinstance(
+            context.current_scenario, ScenarioVariantModel
+        ), "load_bhv_impl: 'model_graph' not a rdflib.Graph"
 
-        bhv_impl = BehaviourImplModel(graph=context.model_graph, bhv_impl_uri=bhv_impl_ids[0])
+        scr_var_id = context.current_scenario.id
+        if scr_var_id in self._bhv_impl_by_scr_var:
+            return self._bhv_impl_by_scr_var[scr_var_id]
+
+        scr_exec_id = self._scr_exec_graph.value(
+            predicate=URI_BDD_PRED_OF_VARIANT, object=scr_var_id, any=False
+        )
+        assert isinstance(
+            scr_exec_id, URIRef
+        ), f"'{scr_var_id}' doesn't link to a ScenarioExecution URI: {scr_exec_id}"
+        scr_exec_model = ModelBase(node_id=scr_exec_id, graph=self._scr_exec_graph)
+        self._scr_exec_by_scr_var[scr_var_id] = scr_exec_model
+
+        bhv_impl_id = self._scr_exec_graph.value(
+            subject=scr_exec_id, predicate=URI_BDD_PRED_HAS_BHV_IMPL, any=False
+        )
+        assert isinstance(
+            bhv_impl_id, URIRef
+        ), f"ScenarioExecution '{scr_exec_id}' doesn't link to a BehaviourImplementation URI: {bhv_impl_id}"
+        bhv_impl = BehaviourImplModel(graph=self._scr_exec_graph, bhv_impl_id=bhv_impl_id)
+
         self.bhv_model_loader.load_attributes(graph=context.model_graph, model=bhv_impl)
 
         if URI_PY_TYPE_MODULE_ATTR in bhv_impl.types:
@@ -95,7 +125,9 @@ class ExecutionModel(object):
             ), f"Implementation for '{bhv_impl.id}' is not an extension of '{Behaviour}'"
             bhv_impl.behaviour = bhv_cls(context=context, **kwargs)
 
-        assert bhv_impl.behaviour is not None
+        assert (
+            bhv_impl.behaviour is not None
+        ), f"no behaviour type handled for BehaviourImpl '{bhv_impl.id}', types: {bhv_impl.types}"
 
-        self._bhv_impl[bhv_id] = bhv_impl
+        self._bhv_impl_by_scr_var[scr_var_id] = bhv_impl
         return bhv_impl
