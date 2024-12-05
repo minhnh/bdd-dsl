5a1d6e7ffbedf184df89d912abf1812536832602
Minh Nguyen <minh.nguyen@uni-bremen.de>
2024-12-05 15:17:50 +0100

    track behave environment for mockup execution
    - add environment module and import necessary steps
    - track *.py file under `examples/generated`
    - exclude `examples/generated` in ruff check

---
+++ examples/generated/steps/imported_steps.py
@@ -0,0 +1,3 @@
+from bdd_dsl.execution.mockup import (
+    before_scenario, given_objects_mockup, given_agents_mockup, given_ws_models, given_scene_mockup, is_located_at_mockup_given, behaviour_mockup
+)
